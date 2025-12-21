efa1de1a77ebfae5799c2f52790a02605ffbe40a
ErnestoUSask <133558417+ErnestoUSask@users.noreply.github.com>
2025-12-20 22:09:20 -0600

    Delete adorym_simplified directory

--- adorym_simplified/adorym_slim/core/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Core reconstruction routines."""
-
-from .recon import reconstruct_ptychography
-from .optim import AdamOptimizer
-
-__all__ = ["reconstruct_ptychography", "AdamOptimizer"]
--- adorym_simplified/adorym_slim/io/__init__.py
+++
@@ -1,5 +0,0 @@
-"""I/O utilities for the slim reconstruction stack."""
-
-from .data_loaders import DiffractionDataset, load_dataset, load_initial_guess
-
-__all__ = ["DiffractionDataset", "load_dataset", "load_initial_guess"]
